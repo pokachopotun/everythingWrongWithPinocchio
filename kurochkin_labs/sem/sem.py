bb0f92c6584114cb3264aa5b1e8133a940a325a3
pokachopotun <mr.salixnew@gmail.com>
2018-05-11 05:08:50 +0300

    [upd] фиксируем текущее состояние

--- kurochkin_labs/sem/sem.py
+++ kurochkin_labs/sem/sem.py
@@ -28,38 +28,37 @@ def sem(pts):
             y.append(x)
     y = np.asarray(y)
 
-    n = len(y) # cnt samples
-    k = len(pts) # classes
-    q = len(y[0]) # space size
-    x = np.zeros( (n,k), dtype=np.float64)
-    C = np.random.rand(k, q)
-    R = np.zeros((k, q, q), dtype=np.float64)
-    for i in range(len(R)):
-        R[i] = np.eye(q, q, dtype=np.float64)
+    samples_cnt = len(y)
+    classes_cnt = len(pts)
+    dims = len(y[0]) # space size
+    x = np.zeros( (samples_cnt,classes_cnt), dtype=np.float64)
+    median = np.random.rand(classes_cnt, dims)
+    cov = np.zeros((classes_cnt, dims, dims), dtype=np.float64)
+    for i in range(len(cov)):
+        cov[i] = np.eye(dims, dims, dtype=np.float64)
 
 
     # weights are the same
-    W = np.ones((k, ), dtype=np.float64)
-    W = np.divide(W, k)
+    w = np.ones((classes_cnt, ), dtype=np.float64)
+    w = np.divide(w, classes_cnt)
     eps = 1e-6
-    dst = np.zeros((n, k), dtype=np.float64)
+    dst = np.zeros((samples_cnt, classes_cnt), dtype=np.float64)
     p = np.zeros_like(dst)
     llh = 0.0
     while True:
 
-        DC = np.zeros_like(C)
-        DR = np.zeros_like(R)
-        DW = np.zeros_like(W)
+        DC = np.zeros_like(median)
+        d_cov = np.zeros_like(cov)
+        d_w = np.zeros_like(w)
         dllh  = 0.0
-        for i in range(n):
+        for i in range(samples_cnt):
             sumpi = 0
-            for j in range(k):
-                R_inv = R[j]
+            for j in range(classes_cnt):
+                R_inv = cov[j]
                 for pp in range(len(R_inv)):
-                    R_inv[pp][pp] /= k
-                tmp = (y[i] - C[j]).T.dot(R_inv)
-                dst[i][j] = tmp.dot(y[i] - C[j])
-                p[i][j] = norm_p(q, W[j], dst[i][j], np.linalg.norm(R_inv))
+                    R_inv[pp][pp] /= classes_cnt
+                dst[i][j] = (y[i] - median[j]).T.dot(R_inv).dot(y[i] - median[j])
+                p[i][j] = norm_p(dims, w[j], dst[i][j], np.linalg.norm(R_inv))
                 sumpi += p[i][j]
             x[i] = np.divide(p[i], sumpi)
             dllh += math.log(sumpi)
@@ -67,22 +66,19 @@ def sem(pts):
             xit = xit.reshape((1, len(xit)))
             yi = y[i]
             yi = yi.reshape((len(yi), 1))
-            tmp =  yi.dot(xit).T
-            for a in range(len(tmp)):
-                for b in range(len(tmp[a])):
-                    DC[a][b] += tmp[a][b]
+            tmp2 =  yi.dot(xit).T
+
+            for a in range(len(tmp2)):
+                for b in range(len(tmp2[a])):
+                    DC[a][b] += tmp2[a][b]
             for a in range(len(x[i])):
-                DW[a] += x[i][a]
-        for j in range(k):
-            C[j] = DC[j]/DW[j]
-            for i in range(n):
-                tl = y[i] - C[j]
-                tm = x[i][j]
-                tr = (y[i] - C[j]).T
-                tmp = tl.dot(tm).dot(tr)
-                DR[j] = np.add(DR[j], tmp)
-            R[j] = np.divide(DR[j], n)
-            W[j] = np.divide(DW[j], n)
+                d_w[a] += x[i][a]
+        for j in range(classes_cnt):
+            median[j] = DC[j]/d_w[j]
+            for i in range(samples_cnt):
+                d_cov[j] = np.add(d_cov[j], (y[i] - median[j]).dot(x[i][j]).dot((y[i] - median[j]).T))
+            cov[j] = np.divide(d_cov[j], samples_cnt)
+            w[j] = np.divide(d_w[j], samples_cnt)
 
         if abs(dllh - llh) >= eps:
             llh = dllh
