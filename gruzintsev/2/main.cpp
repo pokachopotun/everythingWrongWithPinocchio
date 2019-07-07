96569e701bedc03dbc1f86ab3fda46e973274097
pokachopotun <mr.salixnew@gmail.com>
2019-07-07 17:33:14 +0300

    2.1

--- gruzintsev/2/main.cpp
+++ gruzintsev/2/main.cpp
@@ -5,18 +5,13 @@ using namespace std;
 class MyClass {
 public:
 	MyClass(int val = 0) : val(val) {};
+	MyClass(bool condition) : val(condition ? 10 : 15) {};
 	int val;
 };
 
 int main() {
 	bool condition = bool(rand() & 1 > 0);
-	MyClass mc;
-	
-	if (condition) {
-		mc = MyClass(10);
-	} else {
-		mc = MyClass(15);
-	}
+	MyClass mc(condition);
 	cout << mc.val << endl;	
 	return 0;
 }
